cfe158d76fbcf92f8604ff404ca549574ea38603
aditya-m-mishra <adiapplies@gmail.com>
2026-05-15 14:40:11 +0530

    birthday match created

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import datetime as dt
+import pandas
+import smtplib
+import random
 
+today = (dt.datetime.now().month, dt.datetime.now().day)
+
+data = pandas.read_csv("birthdays.csv")
+
+birthdays_dict = {
+    (data_row["month"], data_row["day"]): data_row
+    for (index, data_row) in data.iterrows()
+}
+
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents.replace("[NAME]", birthday_person["name"])
